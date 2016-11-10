d895a77f8f9446e3309b5a1a52ab2010bd7c6c56
Rhisereld <laura.mcdonnell@griffithuni.edu.au>
2016-11-10 23:58:31 +1000

    Added all balls with their materials.

---
+++ Pool/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    public float speed = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidBody.AddForce(movement * speed);
+	}
+}
