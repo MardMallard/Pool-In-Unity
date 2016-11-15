be66ee1c1019296c150f13da90dfb8b730f7c345
Rhisereld <laura.mcdonnell@griffithuni.edu.au>
2016-11-15 20:39:23 +1000

    Balls are now deactivated upon being sunk.

---
+++ Pool/Assets/Scripts/SinkBall.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinkBall : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider ball)
+	{
+		ball.gameObject.SetActive(false);
+		Debug.Log("Deactivated ball");
+	}
+}
