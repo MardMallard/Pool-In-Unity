a6df582255ebf1591f5d995d4bfc6fc9218ffe93
Rhisereld <laura.mcdonnell@griffithuni.edu.au>
2016-11-18 13:09:10 +1000

    Introduced the PhysicsCorrector script.
    The script halts and puts to sleep any ball under a certain velocity.

--- Pool/Assets/Scripts/PhysicsCorrector.cs
+++ Pool/Assets/Scripts/PhysicsCorrector.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PhysicsCorrector : MonoBehaviour {
+public class PhysicsCorrector : MonoBehaviour 
+{
+	private Rigidbody rigidBody;
+	public float velocityThreshold;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody>();	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() 
+	{
+		//If the ball is moving under a certain velocity, put it to sleep.
+		if (!rigidBody.IsSleeping() && rigidBody.velocity.magnitude < velocityThreshold)
+		{
+			rigidBody.velocity = Vector3.zero;
+			rigidBody.Sleep();
+		}
 	}
 }
